d6f11f8ee92a146d9d9837c50787ede346ffb8bf
RafatMeraz <razhossain201@gmail.com>
2026-05-16 18:57:05 +0200

    Add basic shape drawing in google maps

--- lib/maps_screen.dart
+++ lib/maps_screen.dart
@@ -27,7 +27,7 @@ class _MapsScreenState extends State<MapsScreen> {
         trafficEnabled: true,
         initialCameraPosition: CameraPosition(
           target: LatLng(23.79934197566539, 90.37194515283755),
-          zoom: 16
+          zoom: 16,
         ),
         onMapCreated: (GoogleMapController controller) {
           _mapController = controller;
@@ -36,12 +36,103 @@ class _MapsScreenState extends State<MapsScreen> {
           print('Dancing animation');
         },
         onCameraMove: (CameraPosition movingPosition) {
-          print(movingPosition.target);
+          // print(movingPosition.target);
         },
         onCameraMoveStarted: () {
           print('Hide Dancing Animation');
         },
-        mapType: .terrain,
+        mapType: .normal,
+        markers: <Marker>{
+          Marker(
+            markerId: MarkerId('home'),
+            position: LatLng(23.800150989663358, 90.37379465997219),
+            infoWindow: InfoWindow(
+              title: 'Home',
+              onTap: () {
+                // TODO: on tapped info window
+              },
+            ),
+          ),
+          Marker(
+            markerId: MarkerId('office'),
+            position: LatLng(23.80321889796988, 90.37127338349819),
+            infoWindow: InfoWindow(
+              title: 'Office',
+              onTap: () {
+                // TODO: on tapped info window
+              },
+            ),
+            icon: BitmapDescriptor.defaultMarkerWithHue(
+              BitmapDescriptor.hueOrange,
+            ),
+            // icon: await BitmapDescriptor.asset(
+            //     ImageConfiguration(), 'path of your icon')
+          ),
+        },
+        circles: <Circle> {
+          Circle(
+            circleId: CircleId('red-zone'),
+            center: LatLng(23.807251763483695, 90.368513725698),
+            radius: 500,
+            fillColor: Colors.red.withAlpha(50),
+            strokeColor: Colors.red,
+            strokeWidth: 3,
+            onTap: () {
+              print('tapped on red-zone circle');
+            },
+            consumeTapEvents: true,
+          ),
+          Circle(
+            circleId: CircleId('green-zone'),
+            center: LatLng(23.79118244110137, 90.37529770284891),
+            radius: 500,
+            fillColor: Colors.green.withAlpha(50),
+            strokeColor: Colors.green,
+            strokeWidth: 3,
+            onTap: () {
+              print('tapped on green-zone circle');
+            },
+            consumeTapEvents: true,
+          )
+        },
+        polylines: <Polyline> {
+          Polyline(
+            polylineId: PolylineId('road-to-office'),
+            points: [
+              LatLng(23.800150989663358, 90.37379465997219),
+              LatLng(23.80321889796988, 90.37127338349819),
+              LatLng(23.800394866637248, 90.3688483312726)
+            ],
+            width: 10,
+            color: Colors.blue,
+            endCap: .roundCap,
+            startCap: .roundCap,
+            visible: true
+          )
+        },
+        polygons: <Polygon> {
+          Polygon(
+            polygonId: PolygonId('danger-area'),
+            points: [
+              LatLng(23.778006586811767, 90.35922825336456),
+              LatLng(23.774350208584387, 90.34503668546677),
+              LatLng(23.76179525763401, 90.35215627402067),
+              LatLng(23.75785027918789, 90.3583263605833),
+              LatLng(23.760235814826416, 90.366489328444),
+              LatLng(23.76607150865706, 90.36912761628628),
+              LatLng(23.77268260579799, 90.36725409328938),
+              LatLng(23.777047783131415, 90.36143001168966),
+              LatLng(23.779566123425163, 90.35585939884186)
+            ],
+            fillColor: Colors.red.withAlpha(50),
+            strokeColor: Colors.orange,
+            strokeWidth: 3,
+            consumeTapEvents: true,
+            onTap: () {
+              print('Tapped on danger area');
+            }
+          )
+        },
       ),
     );
   }
