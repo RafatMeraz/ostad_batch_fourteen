7103714b5a7115703edac8653b8397310d6ae1f3
RafatMeraz <razhossain201@gmail.com>
2026-06-15 19:04:18 +0200

    Add main nav holder screen ui

--- crafty_bay/lib/app/crafty_bay_app.dart
+++ crafty_bay/lib/app/crafty_bay_app.dart
@@ -1,5 +1,6 @@
 import 'package:crafty_bay/app/providers/locale_provider.dart';
 import 'package:crafty_bay/app/providers/theme_mode_provider.dart';
+import 'package:crafty_bay/features/shared/presentation/providers/main_nav_holder_provider.dart';
 import 'package:crafty_bay/l10n/app_localizations.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
@@ -33,6 +34,7 @@ class _CraftyBayAppState extends State<CraftyBayApp> {
       providers: [
         ChangeNotifierProvider.value(value: _themeModeProvider),
         ChangeNotifierProvider.value(value: _localeProvider),
+        ChangeNotifierProvider(create: (_) => MainNavHolderProvider())
       ],
       child: Consumer<LocaleProvider>(
         builder: (context, localeProvider, _) {
--- crafty_bay/lib/app/routes.dart
+++ crafty_bay/lib/app/routes.dart
@@ -4,6 +4,7 @@ import '../features/auth/presentation/screens/sign_in_screen.dart';
 import '../features/auth/presentation/screens/sign_up_screen.dart';
 import '../features/auth/presentation/screens/splash_screen.dart';
 import '../features/auth/presentation/screens/verify_otp_screen.dart';
+import '../features/shared/presentation/presentation/main_nav_holder_screen.dart';
 
 class AppRoutes {
   static Route<dynamic>? onGenerateRoute(RouteSettings settings) {
@@ -18,6 +19,8 @@ class AppRoutes {
         widget = SignUpScreen();
       case VerifyOtpScreen.name:
         widget = VerifyOtpScreen();
+      case MainNavHolderScreen.name:
+        widget = MainNavHolderScreen();
     }
 
     return MaterialPageRoute(builder: (ctx) => widget);
