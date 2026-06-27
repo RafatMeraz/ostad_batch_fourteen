049c1f42ea7a10913188f60e76739a76ae42f8ae
RafatMeraz <razhossain201@gmail.com>
2026-06-27 19:18:57 +0200

    Add cart list screen ui

---
+++ crafty_bay/lib/features/cart/presentation/screens/cart_screen.dart
@@ -0,0 +1,48 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+import '../../../shared/presentation/providers/main_nav_holder_provider.dart';
+import '../widgets/cart_item.dart';
+import '../widgets/total_price_and_checkout_section.dart';
+
+class CartScreen extends StatefulWidget {
+  const CartScreen({super.key});
+
+  @override
+  State<CartScreen> createState() => _CartScreenState();
+}
+
+class _CartScreenState extends State<CartScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return PopScope(
+      onPopInvokedWithResult: (_, _) => _backToHome(),
+      child: Scaffold(
+        appBar: AppBar(
+          title: Text('Cart'),
+          leading: IconButton(
+            onPressed: _backToHome,
+            icon: Icon(Icons.arrow_back_ios),
+          ),
+        ),
+        body: Column(
+          children: [
+            Expanded(
+              child: ListView.builder(
+                itemCount: 4,
+                itemBuilder: (context, index) {
+                  return CartItem();
+                },
+              ),
+            ),
+            TotalPriceAndCheckoutSection(),
+          ],
+        ),
+      ),
+    );
+  }
+
+  void _backToHome() {
+    context.read<MainNavHolderProvider>().backToHome();
+  }
+}
--- crafty_bay/lib/features/shared/presentation/presentation/main_nav_holder_screen.dart
+++ crafty_bay/lib/features/shared/presentation/presentation/main_nav_holder_screen.dart
@@ -1,4 +1,5 @@
 import 'package:crafty_bay/app/app_colors.dart';
+import 'package:crafty_bay/features/cart/presentation/screens/cart_screen.dart';
 import 'package:crafty_bay/features/category/presentation/screens/category_screen.dart';
 import 'package:crafty_bay/features/home/presentation/screens/home_screen.dart';
 import 'package:crafty_bay/features/wishlist/presentation/screens/wishlist_screen.dart';
@@ -20,7 +21,7 @@ class _MainNavHolderScreenState extends State<MainNavHolderScreen> {
   final List<Widget> _screens = const [
     HomeScreen(),
     CategoryScreen(),
-    HomeScreen(),
+    CartScreen(),
     WishlistScreen(),
   ];
 
