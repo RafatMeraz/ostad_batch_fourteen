d4343e941aaa6a3d343ab8d109c7c448d5860058
RafatMeraz <razhossain201@gmail.com>
2026-07-01 19:00:27 +0200

    Refactor shared presentation folder name

--- crafty_bay/lib/app/routes.dart
+++ crafty_bay/lib/app/routes.dart
@@ -6,7 +6,7 @@ import '../features/auth/presentation/screens/splash_screen.dart';
 import '../features/auth/presentation/screens/verify_otp_screen.dart';
 import '../features/products/presentation/screens/product_details_screen.dart';
 import '../features/products/presentation/screens/product_list_by_category_screen.dart';
-import '../features/shared/presentation/presentation/main_nav_holder_screen.dart';
+import '../features/shared/presentation/screens/main_nav_holder_screen.dart';
 
 class AppRoutes {
   static Route<dynamic>? onGenerateRoute(RouteSettings settings) {
